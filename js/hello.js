ae9126fa183d88828cafd2beb7388eebb0e9e4cc
srikanthpragada <srikanthpragada@gmail.com>
2023-07-06 20:57:55 +0530

    06-JUL

---
+++ js/hello.js
@@ -0,0 +1,2 @@
+var msg = "Hello JavaScript"
+alert(msg)
---
+++ js/variables.js
@@ -0,0 +1,15 @@
+
+"use strict"
+
+var msg = "Hello"
+msg = "Good morning"
+console.log(msg)
+
+let uname 
+console.log(uname)
+
+const a = 10
+// a = 20 
+
+let n = 100 
+console.log(n)
