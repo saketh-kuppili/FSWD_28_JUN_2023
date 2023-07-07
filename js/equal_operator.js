66230ee043eae5e1d60c7a3e675eb297fc74d283
srikanthpragada <srikanthpragada@gmail.com>
2023-07-07 20:55:36 +0530

    07-JUL

---
+++ js/equal_operator.js
@@ -0,0 +1,10 @@
+var a = 10
+console.log(typeof(a))
+var b = "10"
+console.log(typeof(b))
+console.log(a == b)
+console.log(a === b)
+
+a = 10
+b = 20 
+console.log( a > b ? a : b)
---
+++ js/while_demo.js
@@ -0,0 +1,7 @@
+var n = 1
+
+while(n <= 10)
+{
+    console.log(n)
+    n ++
+}
