3cfee98a931fd93aef86c14a46b26eb3c479fe65
srikanthpragada <srikanthpragada@gmail.com>
2023-07-08 20:56:45 +0530

    08-JUL

---
+++ js/string_demo.js
@@ -0,0 +1,19 @@
+var st = "Hello"
+
+console.log(st.charAt(0))
+console.log(st.indexOf("L"))
+
+var names = "java,python,c#"
+console.log(names.split(","))
+
+console.log(names.replace(",",":"))
+
+console.log(names.substring(1,4))
+
+var name = "javascript"
+var version = "ES2017"
+console.log( `Name = ${name}, Version = ${version}`)
+
+var s = "133" 
+console.log( parseInt(s) + 12)
+
